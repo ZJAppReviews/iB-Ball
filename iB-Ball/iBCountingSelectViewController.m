6a9ab6df00294c7e52db6cc4a75ae09832123c0c
zzz6519003 <zhengzhongzhao@gmail.com>
2013-05-14 10:54:43 +0800

    shadowed table view, change the table view style

--- iB-Ball/iBCountingSelectViewController.m
+++ iB-Ball/iBCountingSelectViewController.m
@@ -29,6 +29,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    self.tableView.backgroundColor = [UIColor groupTableViewBackgroundColor];
+
 }
 
 - (void)didReceiveMemoryWarning
