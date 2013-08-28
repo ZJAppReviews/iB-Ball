56ce698fdfb0f2899154bc08d972c3ccea382dc0
zzz6519003 <zhengzhongzhao@gmail.com>
2013-08-28 08:02:45 +0800

    revert commit

--- sinaweibo_ios_sdk/iBSkiTableViewController.m
+++ sinaweibo_ios_sdk/iBSkiTableViewController.m
@@ -34,13 +34,18 @@
     self.skillModel = [[iBSkillDataModel alloc] init];
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
- 
+    
+
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     self.navigationItem.rightBarButtonItem = self.editButtonItem;
     
 //    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addItem:)];
 //    self.navigationItem.leftBarButtonItem = addButton;
 
+}
+
+- (void)bbiClicked {
+    [self.tableView moveSection:0 toSection:1];
 }
 
 - (void)didReceiveMemoryWarning
