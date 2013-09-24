c74bd50c55250d07a3ddf4a6ffd3edc9c47af31d
zzz6519003 <zhengzhongzhao@gmail.com>
2013-09-24 08:59:11 +0800

    add game kit

--- iB-Ball/iBTeamMatesViewController.h
+++ iB-Ball/iBTeamMatesViewController.h
@@ -7,8 +7,25 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <GameKit/GameKit.h>
 
-@interface iBTeamMatesViewController : UIViewController
+@interface iBTeamMatesViewController : UIViewController {
+    GKSession *currentSession;
+    IBOutlet UITextField *txtMessage;
+    IBOutlet UIButton *connect;
+    IBOutlet UIButton *disconnect;
+
+}
+
+@property (nonatomic, retain) GKSession *currentSession;
+@property (nonatomic, retain) UITextField *txtMessage;
+@property (nonatomic, retain) UIButton *connect;
+@property (nonatomic, retain) UIButton *disconnect;
+
+-(IBAction) btnSend:(id) sender;
+-(IBAction) btnConnect:(id) sender;
+-(IBAction) btnDisconnect:(id) sender;
+
 
 - (IBAction)AddTeammates:(id)sender;
 
--- iB-Ball/iBTeamMatesViewController.m
+++ iB-Ball/iBTeamMatesViewController.m
@@ -14,6 +14,14 @@
 
 @implementation iBTeamMatesViewController
 
+@synthesize currentSession;
+@synthesize txtMessage;
+@synthesize connect;
+@synthesize disconnect;
+
+GKPeerPickerController *picker;
+
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -27,6 +35,10 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    [connect setHidden:NO];
+    [disconnect setHidden:YES];
+    [super viewDidLoad];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -37,4 +49,112 @@
 
 - (IBAction)AddTeammates:(id)sender {
 }
+
+-(IBAction) btnConnect:(id) sender {
+    picker = [[GKPeerPickerController alloc] init];
+    picker.delegate = self;
+    picker.connectionTypesMask = GKPeerPickerConnectionTypeNearby;
+    [connect setHidden:YES];
+    [disconnect setHidden:NO];
+    [picker show];
+}
+
+- (void)peerPickerController:(GKPeerPickerController *)picker
+              didConnectPeer:(NSString *)peerID
+                   toSession:(GKSession *) session {
+    self.currentSession = session;
+    session.delegate = self;
+    [session setDataReceiveHandler:self withContext:nil];
+    picker.delegate = nil;
+    
+    [picker dismiss];
+}
+
+- (void)peerPickerControllerDidCancel:(GKPeerPickerController *)picker
+{
+    picker.delegate = nil;
+    
+    [connect setHidden:NO];
+    [disconnect setHidden:YES];
+}
+
+-(IBAction) btnDisconnect:(id) sender {
+    [self.currentSession disconnectFromAllPeers];
+    currentSession = nil;
+    
+    [connect setHidden:NO];
+    [disconnect setHidden:YES];
+}
+
+- (void)session:(GKSession *)session
+     peer:(NSString *)peerID
+ didChangeState:(GKPeerConnectionState)state {
+    switch (state)
+    {
+        case GKPeerStateConnected:
+            NSLog(@"connected");
+            break;
+        case GKPeerStateDisconnected:
+            NSLog(@"disconnected");
+            currentSession = nil;
+            
+            [connect setHidden:NO];
+            [disconnect setHidden:YES];
+            break;
+        case GKPeerStateAvailable:
+        case GKPeerStateConnecting:
+        case GKPeerStateUnavailable:
+            break;
+    }
+}
+
+- (void) mySendDataToPeers:(NSData *) data
+{
+    if (currentSession)
+        [self.currentSession sendDataToAllPeers:data
+                                   withDataMode:GKSendDataReliable
+                                          error:nil];
+}
+
+-(IBAction) btnSend:(id) sender
+{
+    //---convert an NSString object to NSData---
+    [txtMessage resignFirstResponder];
+    NSData* data;
+    NSString *str = [NSString stringWithString:txtMessage.text];
+    data = [str dataUsingEncoding: NSASCIIStringEncoding];
+    [self mySendDataToPeers:data];
+}
+
+- (void) receiveData:(NSData *)data
+            fromPeer:(NSString *)peer
+           inSession:(GKSession *)session
+             context:(void *)context {
+    
+    //---convert the NSData to NSString---
+    NSString* str;
+    str = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Data received"
+                                                    message:str
+                                                   delegate:self
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    [alert show];
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
 @end
