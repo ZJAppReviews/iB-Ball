9a64e576870447f47c955c601f573415c0a23e82
zzz6519003 <zhengzhongzhao@gmail.com>
2013-10-09 08:11:38 +0800

    game center helper header

---
+++ iB-Ball/GameKitHelper.h
@@ -0,0 +1,13 @@
+//
+//  GameKitHelper.h
+//  iB-Ball
+//
+//  Created by Snowmanzzz on 13-10-9.
+//  Copyright (c) 2013年 Snowmanzzz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameKitHelper : NSObject
+
+@end
---
+++ iB-Ball/GameKitHelper.m
@@ -0,0 +1,28 @@
+//
+//  GameKitHelper.m
+//  iB-Ball
+//
+//  Created by Snowmanzzz on 13-10-9.
+//  Copyright (c) 2013年 Snowmanzzz. All rights reserved.
+//
+
+// Include the GameKit framework
+
+#import <GameKit/GameKit.h>
+// Protocol to notify external
+// objects when Game Center events occur or
+// when Game Center async tasks are completed
+@protocol GameKitHelperProtocol<NSObject>
+-(void) onAchievementsLoaded:(NSDictionary*)achievements;
+@end
+
+@interface GameKitHelper : NSObject @property (nonatomic, assign)
+id<GameKitHelperProtocol> delegate;
+// This property holds the last known error
+// that occured while using the Game Center API's
+@property (nonatomic, readonly) NSError* lastError;
+// This property holds Game Center achievements
+@property (nonatomic, readonly) NSMutableDictionary* achievements;
++ (id) sharedGameKitHelper;
+// Player authentication, info
+-(void) authenticateLocalPlayer; @end
